567332b8e2e5336453bd4f96b054d37f6b02bc95
brian915 <brian@brianrunk.net>
2016-03-26 22:34:15 -0400

    extended more custom functionality via adding tests and going green, inlcuding custom matcher for ToContainMatch, changed input requirements for base function which now also returns an array

--- spec/matcher_math.spec.js
+++ spec/matcher_math.spec.js
@@ -2,33 +2,92 @@
 //  • toBe checks if two objects are the same.
 //  • toBeTruthy checks if a value is truthy (not just true).
 //  • toBeFalsy checks if a value is falsy (not just false).
-//  • toContain checks if a value is inside another.
 //  • toBeDefined checks if a value is defined.
 //  • toBeUndefined checks if a value is undefined.
 //  • toBeNull checks if a value is null.
 //  • toBeNaN checks if a value is NaN.
+//  • toContain checks if a value is inside another.
 //  • toBeCloseTo checks decimal proximity.
 //  • toMatch checks if a value matches a given regular expression.
 //  • toThrow checks if a function throws an error.
 //  • .not inverts the meaning of the following matcher.
 
+
+
+//TODO:
+// - finish implementing custom match ( and test the "not" case ) 
+// - add the last three matchers ( toBeCloseTo, NaN, Null)
+// - add errors to throw
+// - implement a variety of spies
+// - edit comments at top
+
 require("../src/matcher_math.js");
+
+var a = {
+    foo: 1,
+};
+
+beforeEach(function() {
+    this.addMatchers({
+	toContainMatch: function(expected) {
+	    this.message = function(){
+		return "Expected " + this.actual + " to contain a match";
+	    };
+
+	//  jasmine.Env.prototype.contains_ = function(haystack, needle) {
+	//	if (jasmine.isArray_(haystack)) {
+	//	    for (var i = 0; i < haystack.length; i++) {
+
+	//  REPLACE THIS LINE WITH REGEXP TEST 
+	//		if (this.equals_(haystack[i], needle)) return true;
+	//	    }
+	//	    return false;
+	//	}
+
+	//   RETURN IF NOT AN ARRAY 
+	//	return haystack.indexOf(needle) >= 0;
+	//   };
+
+	    
+
+
+	    //jasmine.Matchers.prototype.toContain = function(expected) {
+	    //	return this.env.contains_(this.actual, expected);
+	    //};
+	    return new RegExp(expected).test(this.actual);
+	}
+    });
+});
 
 describe("testing the behaviour of random number generation", function(){
     it("should pass numerous tests for boolean and truth/falsitude", function(){
-	expect(matcher_math(5)).toBeTruthy();
-	expect(matcher_math(5)).not.toBeFalsy();
-	expect(matcher_math(5)).not.toBeNull();
-	expect(matcher_math('x')).toBeNaN();
+	expect(matcher_math(5,1,4)).toBeTruthy();
+	expect(matcher_math(50000,0,40)).toBeTruthy();
+	expect(matcher_math(5,1,4)).not.toBeFalsy();
+	expect(matcher_math(5,1,4)).not.toBeNull();
 	expect(matcher_math(0)).not.ToBeUndefined;
+	expect(matcher_math(a.foo)).toBeDefined;
+	expect(matcher_math(a.foo).pop()).not.toBeNaN();
+	expect(matcher_math(a.bar)).toBeUndefined;
     });
 
     it("should return a random number within the range of 0 to the input number", function(){
-	expect(matcher_math(0)).toEqual(1);
-	expect(matcher_math(0)).not.toBe(0);
+	expect(matcher_math(0).pop()).toEqual(1);
+	expect(matcher_math(0).pop()).not.toBe(0);
+	expect(matcher_math(8).pop()).toMatch(/[0-9]/);
     });
-    it("should allow an array  to be returned, if an array is provided", function(){
-	//contains
+    
+    it("test the contents of the returned array for a numeric match", function(){
+	//custom combination of contains & matching 
+	expect(matcher_math(9,0,30)).toContainMatch(/[0-9]/);
+	expect(matcher_math(80,0,30)).not.toContainMatch(/[A-Z]/);
+    });
+
+    it("should throw an error on invalid input",function(){
+	// make these throw an error 
+	expect(matcher_math('i').pop()).toBeNaN();
+	expect(matcher_math(a.bar).pop()).toBeNaN();
+	expect(matcher_math(9,0,200,43,69,10)).toBeTruthy();
     });
 });
 
--- src/matcher_math.js
+++ src/matcher_math.js
@@ -1,4 +1,25 @@
-global.matcher_math = function(num){
-    random_num = Math.floor(Math.random() * num) + 1 
-    return random_num;
+global.matcher_math = function(max_num,min_num,nums){
+    if(min_num === undefined){
+	min_num = 1;
+    }
+    if(nums === undefined){
+	nums = 1;
+    }
+    var random_nums = [];
+    
+    for(var x=0; x<=nums; x++){
+	var y = generate_random();
+	random_nums.push(y);
+    }
+
+    return random_nums;
+
+
+    
+    function generate_random(){
+      random_num = Math.floor(Math.random() * max_num) + min_num; 
+      return random_num;
+    }
 }
+
+
